23f065f00b47b889114993ace140e0f6baef7fe2
Hans Trygve <trygvehans@gmail.com>
2023-03-02 17:07:08 +0100

    added fetch for svg files and panzoom library for svg traversal. Added shell functions for updating server status. css changes.

--- src/ServerStatus.js
+++ src/ServerStatus.js
@@ -4,21 +4,37 @@ function ServerStatus() {
     const servers = ["Rust", "Valheim", "etc"];
     var serverDivs = [];
 
-    function restartServer(serverName, passcode){
+    function restartServer(serverName, passcode) {
         console.log("restarting server " + serverName + " with passcode " + passcode);
         //todo create fetch
     }
+
     //todo create if passcode == 1234, open inf rickrolls
-    function restartPrompt(serverName){
+    function restartPrompt(serverName) {
         let passcode = prompt("Enter passcode to restart " + serverName);
-        if(passcode != null && passcode != ""){
+        if (passcode != null && passcode != "") {
             return restartServer(serverName, passcode);
         }
     }
 
+    function updateStatus() {
+        for (let i = 0; i < servers.length; i++) {
+            if (checkStatus(servers[i])) {
+                document.getElementById(servers[i] + "Status").style.backgroundColor = "green";
+            } else {
+                document.getElementById(servers[i] + "Status").style.backgroundColor = "red";
+            }
+        }
+    }
+    setInterval(updateStatus, 1000);
+
+    function checkStatus(serverName) {
+        //todo fetch server status of serverName
+        return true;
+    }
 
     for (let i = 0; i < servers.length; i++) {
-        serverDivs.push(<><div className="server">{servers[i]} </div><div className="circle" /> <button type="button" onClick={function(){restartPrompt(servers[i])}}>Restart</button></>);
+        serverDivs.push(<><div className="server">{servers[i]} </div><div id={servers[i] + "Status"} className="circle" /> <button type="button" onClick={function () { restartPrompt(servers[i]) }}>Restart</button></>);
     }
 
     return (
